0d196c2aa3c0809ada23edf55555dda7b73f52c3
dwiley_contractor <dwiley_contractor@ionisph.com>
2023-05-11 23:04:16 -0600

    ad fun

---
+++ src/AdUnit.js
@@ -0,0 +1,14 @@
+// src/AdUnit.js
+import React from "react";
+
+const AdUnit = () => {
+  const aAdsCode = {
+    __html: `
+    <iframe data-aa='2212934' src='//ad.a-ads.com/2212934?size=728x90' style='width:728px; height:90px; border:0px; padding:0; overflow:hidden; background-color: transparent;'></iframe>
+    `,
+  };
+
+  return <div dangerouslySetInnerHTML={aAdsCode} />;
+};
+
+export default AdUnit;
